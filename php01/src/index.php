93426fabb9e27a0aa359bd70974c825ac484b6bb
suraksha-niveus <103033871+suraksha-niveus@users.noreply.github.com>
2022-07-18 14:50:20 +0530

    Update index.php

--- php01/src/index.php
+++ php01/src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "CICD using argocd and helm <br>";
-echo "Hello ECS! <br>";
+echo "Hello Niveus! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
 ?>
