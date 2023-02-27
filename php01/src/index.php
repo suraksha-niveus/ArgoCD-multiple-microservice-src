41e642c17dd86f5de98deeb2af6fabda9205c173
suraksha.shetty <suraksha.shetty@niveussolutions.com>
2023-02-27 19:55:13 +0530

    update

--- php01/src/index.php
+++ php01/src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "welcome to niveus.........<br>";
+echo "welcome to niveus............<br>";
 echo "Hello all !!!!! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
